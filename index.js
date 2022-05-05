03b64785c22eff7ab557a67ed86b44c75ebb85b1
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-05-05 10:17:01 +0100

    Added animations, movement, collisions

--- index.js
+++ index.js
@@ -6,37 +6,84 @@ const ctx = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-// Setting the position of the canvas and filling it
-ctx.fillStyle = 'white'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+// Map has 70 tiles in width
+const canvasWidth = 70
+const collisionsMap = []
 
-// Creating a new HTML image with the player sprite
-const playerSprite = new Image()
-playerSprite.src = './img/playerDown.png'
+for (let i = 0; i < collisions.length; i += canvasWidth) {
+  collisionsMap.push(collisions.slice(i, canvasWidth + i))
+}
+
+const boundaries = []
+const offset = {
+  x: -735,
+  y: -635
+}
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(new Boundary({ 
+        position: {
+          x: j * Boundary.width + offset.x,
+          y: i * Boundary.height + offset.y
+        }
+      }))
+  })
+})
 
 // Creating a new HTML image with Pellet Town background
 const pelletTown = new Image()
 pelletTown.src = './img/PelletTown.png'
 
-// Sprite class containing constructor for different sprites
-class Sprite {
-  constructor({ position, image }) {
-    this.position = position
-    this.image = image
-  }
+const foregroundImage = new Image()
+foregroundImage.src = './img/foreground.png'
 
-  draw() {
-    ctx.drawImage(this.image, this.position.x, this.position.y)
+// Creating a new HTML image with the player sprite
+const playerSpriteDown = new Image()
+playerSpriteDown.src = './img/playerDown.png'
+
+const playerSpriteUp = new Image()
+playerSpriteUp.src = './img/playerUp.png'
+
+const playerSpriteLeft = new Image()
+playerSpriteLeft.src = './img/playerLeft.png'
+
+const playerSpriteRight = new Image()
+playerSpriteRight.src = './img/playerRight.png'
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2
+  },
+  image: playerSpriteDown,
+  frames: {
+    max: 4
+  },
+  sprites: {
+    up: playerSpriteUp,
+    left: playerSpriteLeft,
+    right: playerSpriteRight,
+    down: playerSpriteDown
   }
-}
+})
 
 // Creating a background sprite for z-axis illusion
 const background = new Sprite({ 
   position: {
-    x: -735,
-    y: -600
+    x: offset.x,
+    y: offset.y
   },
   image: pelletTown
+})
+
+const foreground = new Sprite({ 
+  position: {
+    x: offset.x,
+    y: offset.y
+  },
+  image: foregroundImage
 })
 
 const keys = {
@@ -54,44 +101,170 @@ const keys = {
   }
 }
 
+const movables = [background, ...boundaries, foreground]
+
+function rectangleCollision({ rect1, rect2 }) {
+  return (
+    rect1.position.x + rect1.width >= rect2.position.x && 
+    rect1.position.x <= rect2.position.x + rect2.width &&
+    rect1.position.y <= rect2.position.y + rect2.height &&
+    rect1.position.y + rect1.height >= rect2.position.y
+  )
+}
+
 // Creating an infinite loop for the sprite animations
 function animate() {
   window.requestAnimationFrame(animate)
 
-  // Draws Pellet Town and player at correct positions and cropping when the images load
   background.draw()
-  ctx.drawImage(
-    playerSprite,
-    0,
-    0,
-    playerSprite.width / 4,
-    playerSprite.height,
-    canvas.width / 2 - playerSprite.width / 4 / 2,
-    canvas.height / 2 - playerSprite.height / 2,
-    playerSprite.width / 4,
-    playerSprite.height
-  )
+  boundaries.forEach(boundary => {
+    boundary.draw()
+  })
+  player.draw()
+  foreground.draw()
 
-  if (keys.w.pressed) {
-    background.position.y = background.position.y + 3
+  let moving = true
+  player.moving = false  
+
+  if (keys.w.pressed && lastKey === 'w') {
+    player.moving = true
+    player.image = player.sprites.up
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i]
+
+      if (
+        rectangleCollision({
+          rect1: player,
+          rect2: {
+            ...boundary, 
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y + 3
+            }
+          }
+        })
+      ) {
+        moving = false
+        break
+      }
+    }
+
+    if (moving)
+      movables.forEach(movable => {
+        movable.position.y += 3 
+      })
+  }
+  else if (keys.a.pressed && lastKey === 'a') {
+    player.moving = true
+    player.image = player.sprites.left
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i]
+
+      if (
+        rectangleCollision({
+          rect1: player,
+          rect2: {
+            ...boundary, 
+            position: {
+              x: boundary.position.x + 3,
+              y: boundary.position.y
+            }
+          }
+        })
+      ) {
+        moving = false
+        break
+      }
+    }
+
+    if (moving)
+    movables.forEach(movable => {
+      movable.position.x += 3 
+    })
+  }
+  else if (keys.s.pressed && lastKey === 's') {
+    player.moving = true
+    player.image = player.sprites.down
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i]
+
+      if (
+        rectangleCollision({
+          rect1: player,
+          rect2: {
+            ...boundary, 
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y - 3
+            }
+          }
+        })
+      ) {
+        moving = false
+        break
+      }
+    }
+
+    if (moving)
+    movables.forEach(movable => {
+      movable.position.y -= 3 
+    })
+  }
+  else if (keys.d.pressed && lastKey === 'd') {
+    player.moving = true
+    player.image = player.sprites.right
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i]
+
+      if (
+        rectangleCollision({
+          rect1: player,
+          rect2: {
+            ...boundary, 
+            position: {
+              x: boundary.position.x - 3,
+              y: boundary.position.y
+            }
+          }
+        })
+      ) {
+        moving = false
+        break
+      }
+    }
+
+    if (moving)
+    movables.forEach(movable => {
+      movable.position.x -= 3 
+    })
   }
 }
 
 animate()
 
+let lastKey = ''
+
 window.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'w':
       keys.w.pressed = true
+      lastKey = 'w'
       break
     case 'a':
       keys.a.pressed = true
+      lastKey = 'a'
       break
     case 's':
       keys.s.pressed = true
+      lastKey = 's'
       break
     case 'd':
       keys.d.pressed = true
+      lastKey = 'd'
       break
   }
 })
